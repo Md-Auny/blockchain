59e7125fa52cbcda407f9ad59a70458f22069f7c
Md.Abu Hasnat Auny <37202828+Md-Auny@users.noreply.github.com>
2024-12-11 08:06:11 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const sha256 = require("crypto-js/sha256");
+
+class Block {
+    constructor(timestamp, data, previousHash) {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        }
+
+    calculateHash() {
+        return sha256(
+            this.timestamp + JSON.stringify(this.data) + this.previousHash)
+            .toString();
+
+    };
+}
+
+const block = new Block("2019-01-01", {amount: 5 },"ABCD");
+console.log(block);
